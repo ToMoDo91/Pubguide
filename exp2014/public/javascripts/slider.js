a7e3d12c867e2c6e8403eb3c8c9da804e608cde6
Martin Robertsen <shark_mario@hotmail.com>
2014-11-19 02:23:33 +0100

    Ordnet "slide"-funksjon

--- exp2014/public/javascripts/slider.js
+++ exp2014/public/javascripts/slider.js
@@ -1,46 +1,49 @@
 $(document).ready(function() {
 
+
+	//$('#sider').scrollTo('side1', 0);
 	//get all link with class panel
 	$('a.panel').click(function () {
 
                 //reset and highlight the clicked link
-		$('a.panel').removeClass('selected');
-		$(this).addClass('selected');
+		$('a.panel').removeClass('valgt');
+		$(this).addClass('valgt');
 
-		//grab the current item, to be used in resize function
+		//grab the current side, to be used in resize function
 		current = $(this);
 
                 //scroll it to the destination
-		$('#wrapper').scrollTo($(this).attr('href'), 800);
+		$('#sider').scrollTo($(this).attr('href'), 800);
 
                 //cancel the link default behavior
 		return false;
 	});
 
 
-	//resize all the items according to the new browser size
-	$(window).resize(function () {
 
-		//call the resizePanel function
+	//resize all the sides according to the new browser size
+	/*$(window).resize(function () {
+
+		call the resizePanel function
 		resizePanel();
-	});
+	});*/
 
 });
 
-function resizePanel() {
+/*function resizePanel() {
 
 	//get the browser width and height
 	width = $(window).width();
 	height = $(window).height();
 
-	//get the mask width: width * total of items
-	mask_width = width * $('.item').length;
+	//get the mask width: width * total of sides
+	mask_width = width * $('.side').length;
 
 	//set the dimension
-	$('#wrapper, .item').css({width: width, height: height});
+	$('#sider, .side').css({width: width, height: height});
 	$('#mask').css({width: mask_width, height: height});
 
-	//if the item is displayed incorrectly, set it to the corrent pos
-	$('#wrapper').scrollTo($('a.selected').attr('href'), 0);
+	//if the side is displayed incorrectly, set it to the corrent pos
+	$('#sider').scrollTo($('a.selected').attr('href'), 0);
 
-};
+};*/
