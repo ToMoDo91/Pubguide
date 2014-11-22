d32f40ddfeac9cb62aa83e7ec8ce2a62d7600052
Martin Robertsen <shark_mario@hotmail.com>
2014-11-22 16:46:37 +0100

    Lagt til vurderingssystem

---
+++ exp2014/public/javascripts/rating.js
@@ -0,0 +1,50 @@
+(function(d, t, e, m){
+        // Async Rating-Widget initialization.
+    window.RW_Async_Init = function(){
+
+        RW.init({
+            huid: "203135",
+            uid: "cf67472a8315265abf5225165395ddc9",
+            source: "website",
+            options: {
+                "advanced": {
+                    "layout": {
+                        "align": {
+                            "hor": "center",
+                            "ver": "top"
+                        },
+                        "lineHeight": "16px"
+                    },
+                    "star": {
+                        "stars": 6
+                    },
+                    "font": {
+                        "hover": {
+                            "color": "#CEC5C6"
+                        },
+                        "italic": true,
+                        "color": "#CEC5C6"
+                    }
+                },
+                "size": "medium",
+                "label": {
+                    "background": "#493C3D"
+                },
+                "lng": "no",
+                "style": "crystal_blue"
+            }
+        });
+        RW.render();
+    };
+        // Append Rating-Widget JavaScript library.
+    var rw, s = d.getElementsByTagName(e)[0], id = "rw-js",
+        l = d.location, ck = "Y" + t.getFullYear() +
+        "M" + t.getMonth() + "D" + t.getDate(), p = l.protocol,
+        f = ((l.search.indexOf("DBG=") > -1) ? "" : ".min"),
+        a = ("https:" == p ? "secure." + m + "js/" : "js." + m);
+    if (d.getElementById(id)) return;
+    rw = d.createElement(e);
+    rw.id = id; rw.async = true; rw.type = "text/javascript";
+    rw.src = p + "//" + a + "external" + f + ".js?ck=" + ck;
+    s.parentNode.insertBefore(rw, s);
+    }(document, new Date(), "script", "rating-widget.com/"));
